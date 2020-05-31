44f14994fc89bee36752fb0915498964ee005664
SyudaProject <septianyuda.project@gmail.com>
2020-05-31 14:21:19 +0700

    Setting MySql connection

--- POS/POS.WebApp/Startup.cs
+++ POS/POS.WebApp/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using POS.WebApp.Data;
 
 namespace POS.WebApp
 {
@@ -26,6 +28,7 @@ namespace POS.WebApp
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            services.AddDbContextPool<DataContext>(x => x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
