6175563f23a06a32c00ea9080aadc8c677629456
SyudaProject <septianyuda.project@gmail.com>
2020-07-13 09:50:38 +0700

    commit 13072020

--- POS/POS.WebApp/ClientApp/src/app/login/login.component.ts
+++ POS/POS.WebApp/ClientApp/src/app/login/login.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, OnDestroy, Inject } from '@angular/core';
 import { AuthService } from '../_service/auth.service';
 import { AlertifyService } from '../_service/alertify.service';
 import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy  {
 
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router, @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.renderer.addClass(this.document.body, 'body-login');
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(this.document.body, 'body-login');
   }
 
   login() {
